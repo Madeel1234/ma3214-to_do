6741eae494ae8788a65509f12f5b0a1745ca8451
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-13 12:18:15 +0500

    Add files via upload
    Simple todo list

---
+++ app.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'toDo',
+            message: "What you want to add in your toDos?"
+        },
+        {
+            type: 'confirm',
+            name: 'addMore',
+            message: "Do you want to addMore?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.toDo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
